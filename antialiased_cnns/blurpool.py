c8a952bfd1b33d5f59bb100ec589bd5ff4c456aa
Drew McNutt <anm329@pitt.edu>
2022-02-15 16:50:28 -0500

    Working version of BlurPool3d
    Requires Pytorch version 1.10.0 because it requires ReflectionPad3d

--- antialiased_cnns/blurpool.py
+++ antialiased_cnns/blurpool.py
@@ -16,7 +16,7 @@ class ZeroPad3d(nn.ConstantPad3d):
 
 class BlurPool3d(nn.Module):
     def __init__(self, channels, pad_type='reflect', filt_size=4, stride=2, pad_off=0):
-        super(BlurPool, self).__init__()
+        super(BlurPool3d, self).__init__()
         self.filt_size = filt_size
         self.pad_off = pad_off
         self.pad_sizes = [int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2))] * 3
